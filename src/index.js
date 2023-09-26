88e40baee4e7399b7a3d3b65b491d0304733b53f
Sebastian Bru <sebastiantorresbru@gmail.com>
2023-09-26 11:48:56 -0500

    Comments added

--- src/index.js
+++ src/index.js
@@ -1,61 +1,64 @@
-const express=require("express")
-const app=express()
-const path=require("path")
-const hbs=require("hbs")
-const collection=require("./mongodb")
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb"); // Import the module that manages the MongoDB collection.
 
-const templatePath=path.join(__dirname,'../templates')
+// Define the path to the templates directory.
+const templatePath = path.join(__dirname, '../templates');
 
+// Configure Express.
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",templatePath)
-app.use(express.urlencoded({extended:false}))
+// Routes and controllers.
 
+// Render the home page.
+app.get("/", (req, res) => {
+    res.render("login");
+});
 
-app.get("/",(req,res)=>{
-    res.render("login")
-})
+// Render the signup page.
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
 
-app.get("/signup",(req,res)=>{
-    res.render("signup")
-})
+// Handle the request to register a new user.
+app.post("/signup", async (req, res) => {
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    };
 
-app.post("/signup",async (req,res)=>{
+    // Insert the data of the new user into the collection.
+    await collection.insertMany([data]);
 
-    const data={
-        name:req.body.name,
-        password:req.body.password
-    }
+    // Redirect to the home page.
+    res.render("home");
+});
 
-    await collection.insertMany([data])
+// Handle the login request.
+app.post("/login", async (req, res) => {
+    try {
+        // Find a user in the collection by their name.
+        const check = await collection.findOne({ name: req.body.name });
 
-    res.render("home")
-
-})
-
-app.post("/login",async (req,res)=>{
-
-    try{
-        const check=await collection.findOne({name:req.body.name})
-
-        if(check.password===req.body.password){
-            res.render("home")
+        if (check.password === req.body.password) {
+            // If the password matches, redirect to the home page.
+            res.render("home");
+        } else {
+            // If the password doesn't match, display an error message.
+            res.send("Wrong password");
         }
-        else{
-            res.send("wrong password")
-        }
-
-        res.render("home")
+    } catch {
+        // If an error occurs, display an error message.
+        res.send("Wrong details");
     }
-    catch{
+});
 
-        res.send("wrong details")
-
-    }
-
-})
-
-app.listen(3000, ()=>{
-    console.log("port conected");
-})
+// Start the server on port 3000.
+app.listen(3000, () => {
+    console.log("Port connected");
+});
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,26 +1,27 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-
+// Connect to the MongoDB server running locally on port 27017 and the "qusdb" database.
 mongoose.connect("mongodb://127.0.0.1:27017/qusdb")
-.then(()=>{
-    console.log("mongodb connected")
-})
-.catch(()=>{
-    console.log("failed to connect")
-})
+    .then(() => {
+        console.log("MongoDB connected successfully"); // Log a successful database connection.
+    })
+    .catch(() => {
+        console.log("Failed to connect to MongoDB"); // Log an error if the connection fails.
+    });
 
-const LogInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+// Define the schema for the "users" collection.
+const LogInSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true // Specify that the "name" field is required.
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true // Specify that the "password" field is required.
     }
-})
+});
 
+// Create a model for the "users" collection using the defined schema.
+const collection = new mongoose.model("users", LogInSchema);
 
-const collection=new mongoose.model("users",LogInSchema)
-
-module.exports=collection
+module.exports = collection; // Export the collection model for use in other parts of the application.
